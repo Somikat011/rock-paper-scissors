e25bd867c90539524acc2b1e1222802030580d86
Jai Somkiat <somecat011@gmail.com>
2022-04-01 15:06:51 +1100

    Create functions. Created functions for converting player input and playing a single round of the game.

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@
 
 function computerPlay() {
     //Assign and return random numbers a string value of Rock, Paper or Scissors
+    
     let num = randomNumGenerator();
 
     if (num == 1) {
@@ -15,6 +16,37 @@ function computerPlay() {
 
 function randomNumGenerator() {
     //Generate a random number between 1 and 3
+
     let num = Math.floor((Math.random()*3) + 1);
     return num;
+}
+
+
+function playerSelection(selection) {
+    //Translates input into a desired output
+
+    selection = selection.toLowerCase();
+
+    if (selection == "rock") {
+        return "Rock";
+    } else if (selection == "paper") {
+        return "Paper";
+    } else if (selection == "scissors") {
+        return "Scissors";
+    } else {
+        return "Sorry, that was an invalid choice. Please check your spelling and try again!"
+    }
+}
+
+function playGame(playerSelection, computerSelection) {
+    //Plays a single round of Rock, Paper, Scissors
+
+    if (playerSelection == computerSelection) {
+        return "It's a draw!";
+    } else if ((playerSelection == "Rock" && computerSelection == "Scissors") || (playerSelection == "Paper" 
+    && computerSelection == "Rock") || (playerSelection == "Scissors" && computerSelection == "Paper")) {
+        return `You Win! ${playerSelection} beats ${computerSelection}!`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}!`;;
+    }
 }
